86de6a07793a050bd77a2c941796a489e78519c3
Lorenzo Giusti <lrnz.justy@gmail.com>
2020-01-14 10:07:28 +0100

    Cleaned the code for augumented mnist; now it's plung 'n play

--- augmented_mnist.py
+++ augmented_mnist.py
@@ -23,17 +23,21 @@ epochs = 12
 
 # input image dimensions
 img_rows, img_cols = 28, 28
+img_depth = 1
 
 # the data, split between train and test sets
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-x_train_data = pd.read_csv('mnist_train.csv').to_numpy()
-x_test_data = pd.read_csv('mnist_test.csv').to_numpy()
+#usecols=['odd', 'gefive']
 
+x_train_data = pd.read_csv(r'mnist_train.csv').to_numpy()
+x_test_data = pd.read_csv(r'mnist_test.csv').to_numpy()
+
+num_raw_data_features = x_train_data.shape[1]
 
 x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
 x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
-input_shape = (img_rows, img_cols, 1)
+input_shape = (img_rows, img_cols, img_depth)
 
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
@@ -47,37 +51,34 @@ print(x_test.shape[0], 'test samples')
 y_train = tensorflow.keras.utils.to_categorical(y_train, num_classes)
 y_test = tensorflow.keras.utils.to_categorical(y_test, num_classes)
 
-cnn = Sequential()
-cnn.add(Conv2D(32, kernel_size=(3, 3),
-                 activation='relu',
-                 input_shape=input_shape,
-                 name='conv2d1'))
-cnn.add(Conv2D(64, (3, 3), activation='relu',
-                 name='conv2d2'))
-cnn.add(MaxPooling2D(pool_size=(2, 2),
-                 name='mp1'))
-cnn.add(Dropout(0.25))
-cnn.add(Flatten())
-cnn.add(Dense(128, activation='relu',
-                 name='dense1'))
-cnn.add(Dropout(0.5))
-cnn.add(Dense(num_classes, activation='softmax',
-                 name='dense2'))
+
+cnn = tensorflow.keras.models.Sequential([
+    Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape),
+    Conv2D(64, (3, 3), activation='relu'),
+    MaxPooling2D(pool_size=(2, 2)),
+    Dropout(0.25),
+    Flatten(),
+    Dense(128, activation='relu'),
+    Dropout(0.5),
+    Dense(num_classes, activation='softmax')
+    ])
 
 
+# The final model accepts numerical data on the MLP input and images on the CNN input, outputting a single value
 
 fnn = tensorflow.keras.models.Sequential([
-  tensorflow.keras.layers.Dense(128, input_shape=(12,),activation='relu', name='pino'),
+  tensorflow.keras.layers.Dense(128, input_shape=(num_raw_data_features,),activation='relu'),
   tensorflow.keras.layers.Dropout(0.2),
-  tensorflow.keras.layers.Dense(10, activation='softmax')
+  tensorflow.keras.layers.Dense(num_classes, activation='softmax')
 ])
-
 
 
 combinedInput = concatenate([fnn.output, cnn.output])
 
 x = Dense(4, activation="relu", name='dense3')(combinedInput)
 x = Dense(10, activation="softmax", name='dense4')(x)
+
+
 
 # The final model accepts numerical data on the MLP input and images on the CNN input, outputting a single value
 model = Model(inputs=[fnn.input, cnn.input], outputs=x)
